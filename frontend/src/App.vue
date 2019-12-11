fca7a09d002b812789f538d4e943af4224cf4c9d
manofsteel <yoonsg84@gmail.com>
2019-12-12 00:23:25 +0900

    1. login 2. home 3. router     3.1. 화면 전환 샘플

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,74 @@
 <template>
-    <v-app>
-        <hello-world></hello-world>
-    </v-app>
+    <!-- App.vue -->
+    <div>
+        <template v-if="!loggedIn">
+            <login v-on:login="postLogin"></login>
+        </template>
+        <template v-else>
+            <v-app>
+                <v-navigation-drawer app>
+                    <!-- -->
+                </v-navigation-drawer>
+
+                <v-app-bar app>
+                    <!-- -->
+                </v-app-bar>
+
+                <!-- Sizes your content based upon application components -->
+                <v-content>
+                    <!-- Provides the application the proper gutter -->
+                    <v-container fluid>
+                        <!-- If using vue-router -->
+                        <router-view></router-view>
+                    </v-container>
+                </v-content>
+
+                <v-footer app>
+                    <!-- -->
+                </v-footer>
+            </v-app>
+        </template>
+    </div>
 </template>
 
+<style lang="scss">
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+</style>
 <script>
-import HelloWorld from "./components/HelloWorld";
 
+import Login from './views/Login.vue';
 export default {
-  name: "App",
-
-  components: {
-    "hello-world": HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+    data: function() {
+        return {
+            loggedIn: false
+        }
+    },
+    components: {
+        'login': Login
+    },
+    methods: {
+        postLogin: function() {
+            this.loggedIn = true;
+        }
+    }
 };
 </script>
